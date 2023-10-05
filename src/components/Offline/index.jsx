41514a52968ba7a25eb375815ca13805a8c454f4
Veronika Bendova <svechovaver@gmail.com>
2023-10-05 20:52:15 +0200

    Task done

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,15 +3,34 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
-  if (isOnline) {
-    return (
-      <p>Všechno v pořádku</p>
-    )
+  const handleOffline = () => {
+    console.log('Disconnected from the internet.');
+    setIsOnline(false);
   };
 
-  return (
-    <p>Jste offline!</p>
-  );
+  const handleOnline = () => {
+    console.log('Connected to the internet.');
+    setIsOnline(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener('offline', handleOffline);
+    window.addEventListener('online', handleOnline);
+
+    console.log('mounted');
+
+    return () => {
+      console.log('unmounted');
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+    };
+  }, []);
+
+  if (isOnline) {
+    return <p>Všechno v pořádku</p>;
+  }
+
+  return <p>Jste offline!</p>;
 };
 
 export default Offline;
